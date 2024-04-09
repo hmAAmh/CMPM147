86dba5022be66c629b866f5046bf12f2fc1cea4a
hmAAmh <DatFido@yahoo.com>
2024-04-08 22:03:41 -0700

    Experiment 1 Updates

--- experiment1/js/sketch.js
+++ experiment1/js/sketch.js
@@ -2,78 +2,80 @@
 // Author: Your Name
 // Date:
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+const fillers = {
+  adventurer: [
+    "Blanchard's Cricket Frog ",
+    "Eastern Cricket Frog",
+    "Southern Cricket Frog",
+    "Tusked Frog",
+    "Eastern American Toad",
+    "Boreal Toad",
+    "California Toad",
+    "Arroyo Toad",
+    "Yosemite Toad",
+    "Great Plains Toad",
+    "Western Chihuahuan Green Toad",
+  ],
+  pre: ["old", "new", "dirty"],
+  post: ["pond", "lake", "creek", "river"],
+  people: [
+    "slimy",
+    "large",
+    "flat",
+    "jumpy",
+    "hungry",
+  ],
+  loots: [
+    "flies",
+    "bugs",
+    "worms",
+    "plants",
+  ],
+  baddies: [
+    "lion",
+    "crocodile",
+    "mean frog",
+    "axolotl",
+    "hyena",
+    "dragonfly",
+  ],
+  message: [
+    "croaking",
+    "eating",
+    "jumping",
+    "sleeping",
+  ],
+};
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+const template = `I just stumbled across a $adventurer who is $message right of the coast of the $pre
+$post! It's look especially $people, and like it's probably trying to hunt for $loots. 
+I hope it doesn't run into a $baddies.
+`;
 
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
+// STUDENTS: You don't need to edit code below this line.
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+const slotPattern = /\$(\w+)/;
 
-    myMethod() {
-        // code to run when method is called
-    }
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
+  }
 }
 
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
+
+  /* global box */
+  box.innerText = story;
 }
 
-// setup() function is called once when the program starts
-function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
+/* global clicker */
+clicker.onclick = generate;
 
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
-}
-
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
+generate();
