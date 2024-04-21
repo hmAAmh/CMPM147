5288547645fc82d94343003f4f6cfcd676d2a3b0
hmAAmh <DatFido@yahoo.com>
2024-04-21 16:53:16 -0700

    confused

--- experiment3/js/project.js
+++ experiment3/js/project.js
@@ -1,34 +1,125 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+/* exported preload, setup, draw, placeTile */
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
+/* global generateGrid drawGrid */
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
+let seed = 0;
+let tilesetImage;
+let currentGrid = [];
+let numRows, numCols;
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
-  }
+function preload() {
+  tilesetImage = loadImage(
+    "https://cdn.glitch.com/25101045-29e2-407a-894c-e0243cd8c7c6%2FtilesetP8.png?v=1611654020438"
+  );
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+function reseed() {
+  seed = (seed | 0) + 1109;
+  randomSeed(seed);
+  noiseSeed(seed);
+  select("#seedReport").html("seed " + seed);
+  regenerateGrid();
 }
 
-// let's get this party started - uncomment me
-//main();
+function regenerateGrid() {
+  select("#asciiBox").value(gridToString(generateGrid(numCols, numRows)));
+  reparseGrid();
+}
+
+function reparseGrid() {
+  currentGrid = stringToGrid(select("#asciiBox").value());
+}
+
+function gridToString(grid) {
+  let rows = [];
+  for (let i = 0; i < grid.length; i++) {
+    rows.push(grid[i].join(""));
+  }
+  return rows.join("\n");
+}
+
+function stringToGrid(str) {
+  let grid = [];
+  let lines = str.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    let row = [];
+    let chars = lines[i].split("");
+    for (let j = 0; j < chars.length; j++) {
+      row.push(chars[j]);
+    }
+    grid.push(row);
+  }
+  return grid;
+}
+
+function setup() {
+  numCols = select("#asciiBox").attribute("rows") | 0;
+  numRows = select("#asciiBox").attribute("cols") | 0;
+
+  createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer");
+  select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
+
+  select("#reseedButton").mousePressed(reseed);
+  select("#asciiBox").input(reparseGrid);
+
+  reseed();
+}
+
+
+function draw() {
+  randomSeed(seed);
+  drawGrid(currentGrid);
+}
+
+function placeTile(i, j, ti, tj) {
+  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
+}
+
+function gridCheck(grid, i, j, target) {
+
+  if(i >= 0 && i < grid.length && j >= 0  && j < grid[i].length)
+  {
+    if(grid[i][j] == target)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+function gridCode(grid, i, j, target) {
+  let bitNorth = gridCheck(grid, i, j - 1, target);
+  let bitSouth = gridCheck(grid, i, j + 1, target);
+  let bitWest  = gridCheck(grid, i - 1, j, target);
+  let bitEast  = gridCheck(grid, i + 1, j, target);
+
+  return (bitNorth<<0)+(bitSouth<<1)+(bitEast<<2)+(bitWest<<3);
+}
+
+function drawContext(grid, i, j, target, ti, tj) {
+  code = gridCode(grid, i, j, target);
+  print("______");
+  print("Code: " + code);
+  print("Lookup: " + lookup[code]);
+  const [tiOffset, tjOffset] = lookup[code];
+  placeTile(i, j, ti + tiOffset, tj + tjOffset);
+}
+
+const lookup = [
+  [1,1],
+  [2,1],
+  [3,1],
+  [4,1],
+  [5,1],
+  [6,1],
+  [7,1],
+  [8,1],
+  [9,1],
+  [10,1],
+  [11,1],
+  [12,1],
+  [13,1],
+  [14,1],
+  [15,1],
+  [16,1]
+];
--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,79 +1,112 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+/* exported generateGrid, drawGrid */
+/* global placeTile */
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
+function generateGrid(numCols, numRows) {
+  let grid = [];
+  for (let i = 0; i < numRows; i++) {
+    let row = [];
+    for (let j = 0; j < numCols; j++) {
+      if(j % 2 == 0)
+      {
+        row.push("_");
+      }
+      else
+      {
+        row.push(".");
+      }
     }
+    grid.push(row);
+  }
+  
+  return grid;
+}
 
-    myMethod() {
-        // code to run when method is called
+function drawGrid(grid) {
+  background(128);
+  //populateRooms(5, 3, 6, grid);
+  
+  for(let i = 0; i < grid.length; i++) {
+    for(let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] == '_') {
+        //placeTile(i, j, (floor(random(4))), 0);
+      }
+      if (gridCheck(grid, i, j, ".")) {
+        placeTile(i, j, floor(random(4)), 0);
+      } else {
+        drawContext(grid, i, j, ".", 4, 0);
+      }
+
     }
+  }
+  
+  gridCode(grid, 0, grid[0].length - 1, grid[0][0]);
+  
 }
 
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
+function drawRoom(grid, roomX, roomY, roomXSize, roomYSize) 
+{
+  
+  let roomXEnd = roomX + roomXSize;
+  let roomYEnd = roomY + roomYSize;
+  for(let i = roomY; i < roomYEnd; i++) {
+    for(let j = roomX; j < roomXEnd; j++) {
+      
+      //placeTile(i, j, 20, 23); //Default Tiles
+      placeTile(i, j, 20, 23);
+      
+      if(i == roomY) //Top
+      {
+        placeTile(i, j, 26, 21);
+      }
+      else if(i == roomYEnd - 2 && j != roomY && j != roomYEnd - 1) // Bottom Corners
+      {
+        placeTile(i, j, 26, 23);
+      }
+      else if(i == roomYEnd - 1) //Bottom
+      {
+        placeTile(i, j, 21, 21 + floor(random(3)));
+      }
+      
+      drawRoomSideWall(i, j, roomX, roomY, roomYEnd, 25, 21);
+      drawRoomSideWall(i, j, roomXEnd - 1, roomY, roomYEnd, 27, 21);
+    }
+  }
 }
 
-// setup() function is called once when the program starts
-function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
+function drawRoomSideWall(i, j, roomX, roomY, roomYEnd, tileX, tileY)
+{
+  //drawRoom helper function, renders the corners of rooms
+  if(j == roomX)
+  {
+    if(i == roomY) //Top Corner
+    {
+      placeTile(i, j, tileX, tileY);
+    }
+    else if(i < roomYEnd - 2) //Side
+    {
+      placeTile(i, j, tileX, tileY + 1);
+    }
+    else if(i == roomYEnd - 2) //Bottom Corner
+    {
+      placeTile(i, j, tileX, tileY + 2);
+    }  
+  }  
 }
 
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+function populateRooms(numRooms, minSize, maxSize, grid)
+{
+  let curRooms = 0;
+  while(curRooms < numRooms)
+  {
+    
+    //let roomSize = max(floor(random(maxSize)), minSize);
+    let roomX = floor(random(grid.length));
+    let roomY = floor(random(grid[0].length));
+    let roomXSize = floor(random(maxSize)) + minSize;
+    let roomYSize = floor(random(maxSize)) + minSize;
+    
+    drawRoom(grid, roomX, roomY, roomXSize, roomYSize)
+    curRooms++;
+  }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
